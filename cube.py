5fb56b67b0e333e173983f145254e4eaaa7c9309
jasperwreed <jaspermreed@gmail.com>
2021-08-10 00:36:54 -0700

    Cube: draw & draw_change

--- cube.py
+++ cube.py
@@ -14,6 +14,39 @@ class Cube:
         self.height = height
         self.selected = False
 
+    def draw(self, window):
+        font = pygame.font.SysFont("Cascadia Code", 20)
+
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+
+        if self.temp != 0 and self.value == 0:
+            text = font.render(str(self.temp), 1, (128,128,128))
+            window.blit(text, (x+5, y+5))
+        elif not(self.value == 0):
+            text = font.render(str(self.value), 1, (0, 0, 0))
+            window.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
+
+        if self.selected:
+            pygame.draw.rect(window, (0,255,255), (x,y, gap ,gap), 3)
+
+    def draw_change(self, window, g=True):
+        font = pygame.font.SysFont("Cascadia Code", 20)
+
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+
+        pygame.draw.rect(window, (255, 255, 255), (x, y, gap, gap), 0)
+
+        text = font.render(str(self.value), 1, (0, 0, 0))
+        window.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
+        if g:
+            pygame.draw.rect(window, (0, 255, 0), (x, y, gap, gap), 3)
+        else:
+            pygame.draw.rect(window, (255, 0, 0), (x, y, gap, gap), 3)
+
     def set(self, value):
         self.value = value
 
