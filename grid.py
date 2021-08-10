0844ea3b1b6889a8facfbde35b56183ed2a8daaf
jasperwreed <jaspermreed@gmail.com>
2021-08-10 00:44:54 -0700

    Grid: solve functions in place

--- grid.py
+++ grid.py
@@ -20,4 +20,52 @@ class Grid:
         self.update_model()
         self.selected = None
 
-    
+    def update_model(self):
+        self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
+
+    def solve(self):
+        find = find_zero(self.model)
+        if not find:
+            return True
+        else:
+            row, col = find
+
+        for i in range(1, 10):
+            if is_valid(self.model, i, row, col):
+                self.model[row][col] = i
+
+                if self.solve():
+                    return True
+
+                self.model[row][col] = 0
+
+        return False
+
+    def solve_gui(self):
+        self.update_model()
+        find = find_zero(self.model)
+        if not find:
+            return True
+        else:
+            row, col = find
+
+        for i in range(1, 10):
+            if is_valid(self.model, i, row, col):
+                self.model[row][col] = i
+                self.cubes[row][col].set(i)
+                self.cubes[row][col].draw_change(self.window, True)
+                self.update_model()
+                pygame.display.update()
+                pygame.time.delay(100)
+
+                if self.solve_gui():
+                    return True
+
+                self.model[row][col] = 0
+                self.cubes[row][col].set(0)
+                self.update_model()
+                self.cubes[row][col].draw_change(self.window, False)
+                pygame.display.update()
+                pygame.time.delay(100)
+
+        return False
