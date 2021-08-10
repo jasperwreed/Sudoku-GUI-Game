f5da882448f00cca4506e268cd6057fffe294a4c
jasperwreed <jaspermreed@gmail.com>
2021-08-10 00:43:46 -0700

    Grid: picking random board and init done

--- grid.py
+++ grid.py
@@ -6,4 +6,18 @@ from boards import *
 from solver import *
 
 class Grid:
-    pass
+    
+    board = random.sample(boards, k=1)[0]
+
+    def __init__(self, rows, cols, width, height, window):
+        self.rows = rows
+        self.cols = cols
+        self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
+        self.width = width
+        self.height = height
+        self.window = window
+        self.model = None
+        self.update_model()
+        self.selected = None
+
+    
